1cf2df90fa91853e2573e2863178cb4acc31ac5d
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2021-12-30 21:33:11 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,59 +3,59 @@ import { graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 // markup
-const IndexPage = ({ data }) => {
-  const {
-    strapi: {
-      homeContent: {
-        data: {
-          attributes: { Content },
-        },
-      },
-    },
-  } = data;
+const IndexPage = () => {
+  // const {
+  //   strapi: {
+  //     homeContent: {
+  //       data: {
+  //         attributes: { Content },
+  //       },
+  //     },
+  //   },
+  // } = data;
 
-  const image = getImage(Content[0].Media.data.attributes.imageFile);
+  // const image = getImage(Content[0].Media.data.attributes.imageFile);
 
   return (
     <main>
-      <h1 className='text-yellow-300'>{Content[0].Titel}</h1>
-      <GatsbyImage image={image} />
+      {/* <h1 className='text-yellow-300'>{Content[0].Titel}</h1>
+      <GatsbyImage image={image} /> */}
       test hallo
     </main>
   );
 };
 
-export const query = graphql`
-  {
-    strapi {
-      homeContent {
-        data {
-          attributes {
-            Content {
-              ... on STRAPI_ComponentContentMediaTekst {
-                id
-                Tekst
-                Titel
-                Media {
-                  data {
-                    attributes {
-                      url
-                      imageFile {
-                        id
-                        childImageSharp {
-                          gatsbyImageData
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   {
+//     strapi {
+//       homeContent {
+//         data {
+//           attributes {
+//             Content {
+//               ... on STRAPI_ComponentContentMediaTekst {
+//                 id
+//                 Tekst
+//                 Titel
+//                 Media {
+//                   data {
+//                     attributes {
+//                       url
+//                       imageFile {
+//                         id
+//                         childImageSharp {
+//                           gatsbyImageData
+//                         }
+//                       }
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
 
 export default IndexPage;
