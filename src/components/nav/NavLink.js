7222c0e3ccfbd183c8d8ec65fe177c55b3df6f87
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2021-12-31 09:24:48 +0100

    Fix spark hover

--- src/components/nav/NavLink.js
+++ src/components/nav/NavLink.js
@@ -2,18 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
+import SparkHover from '../../images/spark-hover.svg';
 
 const NavLink = ({ to, text }) => {
   return (
     <Link className='text-navlink ml-16 font-medium group relative' to={to}>
       <span className='relative z-20'>{text}</span>
 
-      <StaticImage
-        src='../../images/spark-hover.svg'
+      <img
+        src={SparkHover}
         alt='Spark hover'
-        placeholder='blurred'
-        layout='constrained'
-        loading='eager'
         className='absolute z-10 opacity-0  transition-opacity group-hover:opacity-100 -top-1 left-1/2 transform -translate-x-1/2 rotate-12'
       />
     </Link>
